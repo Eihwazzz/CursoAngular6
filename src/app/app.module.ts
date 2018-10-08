af03eef83898f4e9ffd89462eaa6da8c7b99c452
Your Name <you@example.com>
2018-10-08 20:46:38 -0300

    Se agrega share de modulos y router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ItemsModule } from './items/items.module';
+import { HomeModule } from './home/home.module';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 import { AlumnoComponent } from './alumno/alumno.component';
 import { DocenteComponent } from './docente/docente.component';
 import { CursoComponent } from './curso/curso.component';
+import { ItemsComponent } from './items/items/items.component';
+import { HomeComponent } from './home/home/home.component';
+
+const ROUTES: Routes = [
+  { path: 'items', component: ItemsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +28,10 @@ import { CursoComponent } from './curso/curso.component';
     CursoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ItemsModule,
+    HomeModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
